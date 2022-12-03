49697c07bc73fd3f5e0df41a91fa0cec39d4597c
eduardommera <117490801+eduardommera@users.noreply.github.com>
2022-12-03 13:41:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,47 +5,61 @@ Created on Fri Nov  4 19:17:10 2022
 @author: Eduardo
 """
 
-from fastapi import FastAPI, UploadFile, File, BackgroundTasks
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pickle
 import json
-import pandas as pd
 
-#from fastapi.responses import StreamingResponse
-#import io
-
-import csv
-import codecs
 
 
 app = FastAPI()
 
-#class file_input(BaseModel):
-        
+
+
+ origins = ["*"]
+ app.add_middleware(CORSMiddleware,
+                    allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+    
+ )
+
+
+class model_input(BaseModel):
+    d_asignatura :int
+    estado :int
+    e_calificacion :int
+    nota :int
+    genero :int
+    edad :int
+    financiamiento :int
+    trabaja :int
+    region :int
+    nacionalidad :int
+    nepadre :int
+    nemadre :int
+    nem :int
+    rprocedencia :int
+    recidencia :int
+    aextra :int
+    rreprobados :int
+    hijos :int
+    ecivil :int
+    apoderado :int
+    hermanos :int
+    horario :int
+
+
+
+#carga del modelo guardado
 deser_model = pickle.load(open('modelo_desercion.sav','rb'))
 
-@app.post('/csv')
-async def root(file: UploadFile = File(...)):
-    return{"file_name":file.filename}
-
-
-@app.post("/upload")
-def upload( file: UploadFile = File(...)):
-    csvReader = csv.DictReader(codecs.iterdecode(file.file, 'latin-1'))
-  
-    return list(csvReader)
-
- 
-
-@app.post("/upload2")
-def upload( file: UploadFile = File(...)):
-    csvReader = csv.DictReader(codecs.iterdecode(file.file, 'latin-1'))
-  
-    #return list(csvReader)
-    input_parameters =list(csvReader)
+@app.post('/prediccion_desercion')
+def deser_pred(input_parameters :model_input):
     input_data = input_parameters.json()
-    input_dictionary = json.loads(input_data)    
+    input_dictionary = json.loads(input_data)
     
     asig = input_dictionary['d_asignatura']
     est = input_dictionary['estado']
@@ -69,24 +83,15 @@ def upload( file: UploadFile = File(...)):
     apode = input_dictionary['apoderado']
     herma = input_dictionary['hermanos']
     hora = input_dictionary['horario']
-    
-    
+
+
     input_list =[asig, est, califi, grade, gend, age, finan, work, reg, nacio, nepa, nema, nemscore, 
                  rproce, reci, actextra, rreprob, hijo, ecivi, apode, herma, hora]
     
     
     prediccion = deser_model.predict([input_list])
     
-    
-    return list(prediccion)
-
-
-
-# deser_model.predict(list(csvReader))
-
-
-
-
-
-
-    
+    if prediccion[0] ==0:
+        return 'el alumno no se titula'
+    else:
+        return 'el alumno se titula'
